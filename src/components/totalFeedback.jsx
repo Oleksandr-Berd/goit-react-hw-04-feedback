b823d0406723097d285a0dade9ad41aeb4a6a8e2
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-28 13:50:05 +0300

    1

--- src/components/totalFeedback.jsx
+++ src/components/totalFeedback.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+// import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+// import PropTypes from 'prop-types';
 import css from './TotalFeedback.module.css';
 
-const TotalFeedback = ({ good, neutral, bad }) => (
-  <li className={css.statistic__item}>Total: {good + neutral + bad}</li>
-);
+export default function TotalFeedback({ good, neutral, bad }) {
+  const [total, setTotal] = useState(0);
 
-TotalFeedback.prototype = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
-};
+  useEffect(() => {
+    setTotal(() => good + neutral + bad);
+  }, [good, neutral, bad, total]);
 
-export default TotalFeedback;
+  return <li className={css.statistic__item}>Total: {total}</li>;
+}
+
+// const TotalFeedback = ({ good, neutral, bad }) => (
+//   <li className={css.statistic__item}>Total: {good + neutral + bad}</li>
+// );
+
+// TotalFeedback.prototype = {
+//   good: PropTypes.number.isRequired,
+//   neutral: PropTypes.number.isRequired,
+//   bad: PropTypes.number.isRequired,
+// };
+
+// export default TotalFeedback;
